fd9ea0a77592e4564b634b42e0ffed5a87d41895
Shovon <shovon@cuttingroom>
2022-05-18 19:51:25 +0600

    O(n) solution for contains duplicate

---
+++ Blind75/217_contains_duplicate.py
@@ -0,0 +1,15 @@
+from operator import truediv
+from typing import List
+
+# O(n)
+def containsDuplicate(nums: List[int]) -> bool:
+    tracker = dict()
+
+    for num in nums:
+      if num in tracker:
+        return True
+      tracker[num] = True
+
+    return False
+
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
