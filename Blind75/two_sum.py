1befdaba9fb19c7b234d40c643c63e97b8564dc8
Shovon <shovon@cuttingroom>
2022-05-18 19:32:29 +0600

    O(n) solution for two sum

--- Blind75/two_sum.py
+++ Blind75/two_sum.py
@@ -1,9 +1,22 @@
 from typing import List
 
+# O(n^2)
 def twoSum(nums: List[int], target: int) -> List[int]:
   for i in range(len(nums)):
       for j in range(len(nums)):
           if i != j and nums[i] + nums[j] == target:
               return [i, j]
+
+print(twoSum([1, 2, 3, 4, 5], 6))
+
+# O(n)
+def twoSum(nums: List[int], target: int) -> List[int]:
+  hash_map = dict()
+  for i in range(len(nums)):
+    required = target - nums[i]
+    if required in hash_map:
+      return [i, hash_map[required]]
+
+    hash_map[nums[i]] = i
 
 print(twoSum([1, 2, 3, 4, 5], 6))
